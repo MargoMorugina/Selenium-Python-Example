e0c1be03016ae5de58c8ca5dc0b5a73cd3a5ebc7
MargoMorugina <mv.morugins@gmail.com>
2023-10-16 11:37:29 +0200

    add test for searchadding tests to check the search on the page expertlist

---
+++ tests/expertlist_test.py
@@ -0,0 +1,31 @@
+import json
+import os
+from datetime import datetime
+import time
+from pathlib import Path
+import allure
+import pytest
+from assertpy import assert_that
+from tests.base_test import BaseTest
+
+@pytest.mark.security
+class TestExpertlist(BaseTest):
+    def test_search_by_profile_type(self, json_data: dict):
+         profile_type = json_data["profile_types"]["profile_type_operator"]
+         specialist_country = json_data["specialist_country"]["country_name_1"]
+         self.consultant_page.consultant_login(os.getenv("EMAIL_CONS"), os.getenv("PASSWORD_CONS"))
+         self.expertlist_page.select_menu_market()
+         time.sleep(3)
+         self.expertlist_page.search_profile()
+         self.specialist_select_page.select_profile_type_list(profile_type)
+         time.sleep(5)
+         self.expertlist_page.search_btn()
+
+
+    def test_search_by_gender(self, json_data: dict):
+         self.consultant_page.consultant_login(os.getenv("EMAIL_CONS"), os.getenv("PASSWORD_CONS"))
+         self.expertlist_page.select_menu_market()
+         time.sleep(3)
+         self.expertlist_page.femail_btn()
+         time.sleep(1)
+         self.expertlist_page.search_btn()
